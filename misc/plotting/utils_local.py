f932b9dbe1a3cccdd49b6941bef9a41b072184c6
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2022-01-31 21:03:29 +0100

    Important: Adds support to load CSVs directly from Google spreadhseets

--- misc/plotting/utils_local.py
+++ misc/plotting/utils_local.py
@@ -2,12 +2,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 
-colors_v1 = ["silver", "hotpink", "gold", "yellowgreen", "cornflowerblue", "purple", "blue", "red", "darkkhaki", "black"]
+colors_v1 = ["silver", "hotpink", "gold", "yellowgreen", "cornflowerblue", "purple", "blue", "red", "darkkhaki", "limegreen", "black"]
 colors_v2 = ["gray", "gold", "skyblue", "green", "darkorange", "lightseagreen", "blue", "red", "magenta", "black"]
 colors_v3 = ["darkgray", "violet", "indigo", "blue", "green", "gold", "orange", "red", "magenta", "black"]
 colors_v4 = ["lightgray", "gold", "skyblue", "green", "darkorange", "lightseagreen", "blue", "red", "magenta", "black"]
+
+
+BASE_URL = 'https://docs.google.com/spreadsheets/d/'
+SHEET_ID = "1PU5rWDZYtIdxdoXRh1EmhFtmNOazvWkt3iV30FJLgdA"
+GID = {
+    "granularity": "733587682",
+    "domain_shift_linear": "2014896504",
+    "domain_shift_finetune": "890573593",
+    "dataset_size": "1134984848",
+}
+
+
+def read_spreadsheet(sheet_id=SHEET_ID, gid=None, gid_key="granularity", **kwargs):
+    if gid is None:
+        gid = GID[gid_key]
+
+    df = df = pd.read_csv(BASE_URL + SHEET_ID + f'/export?gid={gid}&format=csv', **kwargs)
+    return df
 
 
 def heatmap(df, titlesize=20, labelsize=15, tickssize=13):
